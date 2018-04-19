446a9db2177012ba866a4bc2ad3f5ee4650da242
Ivson Santos <santosivson@hotmail.com>
2018-04-19 20:35:42 -0300

    Player HealthBar

---
+++ Assets/scripts/BillBoard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillBoard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// keep the Healthbar looking at the Main Camera
+		transform.LookAt(Camera.main.transform);
+	}
+}
