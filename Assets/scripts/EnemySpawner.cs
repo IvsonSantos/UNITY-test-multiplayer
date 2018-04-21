ac1301f6b990fbd8f67c17c82465f04884d2e6c1
Ivson Santos <santosivson@hotmail.com>
2018-04-21 12:18:15 -0300

    Criado 4 inimigos

---
+++ Assets/scripts/EnemySpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class EnemySpawner : NetworkBehaviour  {
+
+	public GameObject enemyPrefab;
+	public int numberOfEnemies;
+
+	// The class uses an implementation of the virtual function OnStartServer. 
+	// OnStartServer is called on the Server when the Server starts listening to the Network.
+	public override void OnStartServer()
+	{
+		// When the server starts, a number of enemies are generated at a random position and rotation and then these are spawned using NetworkServer.Spawn(enemy)
+		for (int i=0; i < numberOfEnemies; i++)
+		{
+			var spawnPosition = new Vector3(
+				Random.Range(-8.0f, 8.0f),
+				0.0f,
+				Random.Range(-8.0f, 8.0f));
+
+			var spawnRotation = Quaternion.Euler( 
+				0.0f, 
+				Random.Range(0,180), 
+				0.0f);
+
+			var enemy = (GameObject)Instantiate(enemyPrefab, spawnPosition, spawnRotation);
+			NetworkServer.Spawn(enemy);
+		}
+	}
+}
